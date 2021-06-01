389b34e90832b88968c5aa3927a83a5c79f2a2a3
NeedGlasses <jhiy2004@gmail.com>
2021-06-01 20:31:01 -0300

    Adicionando os arquivos iniciais

---
+++ _conexao_banco/conexao.php
@@ -0,0 +1,11 @@
+<?php
+    $servidor = "localhost";
+    $usuario  = "root";
+    $senha    = "";
+    $banco    = "projetoeletiva";
+    $conecta  = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+    if(mysqli_connect_errno()) {
+        die("Conexão Falhou: ".mysqli_connect_errno());
+    }
+?>
