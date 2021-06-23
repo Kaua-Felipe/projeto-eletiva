808ffc5e83a401ee30aceffa1bd1ad1c899eb4f7
Kauã <kauafelipeoficial191@gmail.com>
2021-06-23 11:32:43 -0300

    Commit para implementar o upload de imagens

---
+++ include/navegacao.php
@@ -0,0 +1,6 @@
+<div class="navbar">
+    <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Home</a> 
+    <a href="#"><i class="fa fa-fw fa-search"></i> Search</a> 
+    <a href="#"><i class="fa fa-fw fa-envelope"></i> Contact</a> 
+    <a href="#"><i class="fa fa-fw fa-user"></i> Login</a>
+</div>
