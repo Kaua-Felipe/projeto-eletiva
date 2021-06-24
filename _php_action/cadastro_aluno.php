66f73aba8d9cc72edc70a8cdba4e9d3de59b3449
Kauã <kauafelipeoficial191@gmail.com>
2021-06-24 18:49:20 -0300

    Criando sistema da listagem dos alunos, e nova tabela no banco de dados

---
+++ _php_action/cadastro_aluno.php
@@ -0,0 +1,22 @@
+<?php
+    include_once "../_conexao_banco/conexao.php";
+
+    if(empty($_POST["nome_aluno"]) || empty($_POST["data_nascimento"]) || empty($_POST["numero_chamada"])) {
+        // echo "<script>alert('Todos os campos precisam estar preenchidos!')</script>";
+    } else {
+        $nome_aluno      = $_POST["nome_aluno"];
+        $data_nascimento = $_POST["data_nascimento"];
+        $numero_chamada  = $_POST["numero_chamada"];
+
+        // Inserção de aluno no banco
+        $inserir_aluno   = "INSERT INTO alunos (nome_aluno, dataNascimento_aluno, numero_aluno) VALUES ('$nome_aluno', '$data_nascimento', $numero_chamada)";
+        $executar_insercao_aluno = mysqli_query($conecta, $inserir_aluno);
+
+        if(!$executar_insercao_aluno) {
+            die("[ERRO]: Erro na INSERÇÃO!");
+        } else {
+            echo "<script>alert('Inserção realizada com sucesso!')</script>";
+            header("location: ../templates/lista_alunos.php");
+        }
+    }
+?>
